21513ed91f43bc7897eb7aae8e594a64de924501
Théo Alves Dantas <108310203+TheoAlves@users.noreply.github.com>
2024-11-23 01:10:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,41 @@
+// Carrossel
 let currentIndex = 0;
-const persons = document.querySelectorAll('.carousel .person');
+const persons = document.querySelectorAll('.carrossel_integrantes .integrantes');
 const totalPersons = persons.length;
 const visiblePersons = 3;
 
-// Carrossel
 function moveCarousel(direction) {
   currentIndex += direction;
 
   if (currentIndex < 0) {
-    currentIndex = totalPersons - visiblePersons; 
+    currentIndex = totalPersons - visiblePersons;
   } else if (currentIndex >= totalPersons - visiblePersons + 1) {
     currentIndex = 0;
   }
 
   const newTransform = `translateX(-${(currentIndex * 100) / visiblePersons}%)`;
-  document.querySelector('.carousel').style.transform = newTransform;
+  document.querySelector('.carrossel_integrantes').style.transform = newTransform;
 }
 
-//Animação Entrada Sobre
+//Animação Entrada Carrossel
 document.addEventListener('DOMContentLoaded', () => {
-  const animationElements = document.querySelectorAll('.sobre');
-
-  const onScroll = () => {
-    const windowHeight = window.innerHeight;
-    const scrollTop = window.scrollY;
-
-    animationElements.forEach(element => {
-      const positionTop = element.getBoundingClientRect().top + scrollTop;
-
-      if (positionTop < windowHeight + scrollTop) {
-        element.classList.add('slide-in-left');
-      }
-    });
-  };
-   window.addEventListener('scroll', onScroll);
-});
-
-//Animação entrada Carrossel
-document.addEventListener('DOMContentLoaded', () => {
-  const carouselContainer = document.querySelector('.carousel-container');
+  const carouselContainer = document.querySelector('.carrossel');
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('show'); 
-        observer.unobserve(entry.target); 
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
       }
     });
   });
 
-  observer.observe(carouselContainer); 
+  observer.observe(carouselContainer);
 });
 
-//Animação Entrada Missão, Visão e Valores
+//Animação De Entradas 
 document.addEventListener('DOMContentLoaded', () => {
-  const animationElements = document.querySelectorAll('.app .image-container, .app .text-container, .conteudo h1, .conteudo p, .conteudo img, .title_depoimentos h3, .title_mascotes h3, .depoimentos .bloco, .depoimentos .bloco p, .safebite, .sobre, .proposito, .depoimentos .bloco h2, .depoimentos .foto, .mascotes .bloco, .mascotes .bloco p, .mascotes .bloco h2, .mascotes .foto');
+  const animationElements = document.querySelectorAll('.app .app_imagem, .app .app_conteudo, .pilares .coluna .conteudo h1, .pilares .coluna .conteudo p, .pilares .coluna .conteudo img, .titulo_depoimentos h1, .titulo_mascotes h1, .depoimentos .bloco, .depoimentos .bloco p, .safebite, .sobre, .proposito, .depoimentos .bloco h2, .depoimentos .foto, .mascotes .bloco, .mascotes .bloco p, .mascotes .bloco h2, .mascotes .foto');
 
   const onScroll = () => {
     const windowHeight = window.innerHeight;
